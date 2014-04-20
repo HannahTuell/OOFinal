bc226859088d67abfdf3aa6449c72e907b77da01
Hannah Tuell <hannah.tuell@colorado.edu>
2014-04-19 22:04:52 -0600

    Added War.java and Pick First Strategy
    War not working, but Class instantiated.

---
+++ src/StrategyPickFirst.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+/**
+ * Created by hannahtuell on 4/19/14.
+ *
+ * Implements a pick the top card from deck Strategy
+ */
+public class StrategyPickFirst implements Strategy {
+
+    public Integer pick_card( List<Card> hand, Suit trump, Suit round ) {
+        return 0;
+    }
+
+}
